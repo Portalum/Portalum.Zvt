ae3e89127ff19fc7c3d278529a8e6853e6d293fd
Tino Hager <tino.hager@nager.at>
2022-02-24 10:16:20 +0100

    Optimize code documentation

--- src/Portalum.Zvt/Models/ByteBitInfo.cs
+++ src/Portalum.Zvt/Models/ByteBitInfo.cs
@@ -1,14 +1,41 @@
 ﻿namespace Portalum.Zvt.Models
 {
+    /// <summary>
+    /// Byte Bit Info
+    /// </summary>
     public class ByteBitInfo
     {
+        /// <summary>
+        /// Bit 0 state
+        /// </summary>
         public bool Bit0 { get; set; }
+        /// <summary>
+        /// Bit 1 state
+        /// </summary>
         public bool Bit1 { get; set; }
+        /// <summary>
+        /// Bit 2 state
+        /// </summary>
         public bool Bit2 { get; set; }
+        /// <summary>
+        /// Bit 3 state
+        /// </summary>
         public bool Bit3 { get; set; }
+        /// <summary>
+        /// Bit 4 state
+        /// </summary>
         public bool Bit4 { get; set; }
+        /// <summary>
+        /// Bit 5 state
+        /// </summary>
         public bool Bit5 { get; set; }
+        /// <summary>
+        /// Bit 6 state
+        /// </summary>
         public bool Bit6 { get; set; }
+        /// <summary>
+        /// Bit 7 state
+        /// </summary>
         public bool Bit7 { get; set; }
     }
 }
