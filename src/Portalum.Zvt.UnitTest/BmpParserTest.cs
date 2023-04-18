060724ec058c1593dd151589cc3803851274e611
Tino Hager <tino.hager@nager.at>
2023-04-18 12:49:46 +0200

    Make static GetDataLengthLL and GetDataLengthLLL

--- src/Portalum.Zvt.UnitTest/BmpParserTest.cs
+++ src/Portalum.Zvt.UnitTest/BmpParserTest.cs
@@ -1,31 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Portalum.Zvt.Parsers;
-using Portalum.Zvt.Repositories;
-using System.Text;
 
 namespace Portalum.Zvt.UnitTest
 {
     [TestClass]
     public class BmpParserTest
     {
-        private BmpParser GetBmpParser()
-        {
-            IErrorMessageRepository errorMessageRepository = new EnglishErrorMessageRepository();
-
-            var logger = LoggerHelper.GetLogger();
-            var tlvParser = new TlvParser(logger.Object);
-            var encoding = Encoding.GetEncoding(437);
-
-            return new BmpParser(logger.Object, encoding, errorMessageRepository, tlvParser);
-        }
-
         [TestMethod]
         public void GetDataLengthLLL_ValidData1_Successful()
         {
             var data = new byte[] { 0xF0, 0xF1, 0xF9 };
 
-            var bmpParser = this.GetBmpParser();
-            var length = bmpParser.GetDataLengthLLL(data);
+            var length = BmpParser.GetDataLengthLLL(data);
 
             Assert.AreEqual(19, length);
         }
@@ -35,8 +21,7 @@ namespace Portalum.Zvt.UnitTest
         {
             var data = new byte[] { 0xF1, 0xF8, 0xF9 };
 
-            var bmpParser = this.GetBmpParser();
-            var length = bmpParser.GetDataLengthLLL(data);
+            var length = BmpParser.GetDataLengthLLL(data);
 
             Assert.AreEqual(189, length);
         }
@@ -46,8 +31,7 @@ namespace Portalum.Zvt.UnitTest
         {
             var data = new byte[] { 0xF1, 0xF8 };
 
-            var bmpParser = this.GetBmpParser();
-            var length = bmpParser.GetDataLengthLL(data);
+            var length = BmpParser.GetDataLengthLL(data);
 
             Assert.AreEqual(18, length);
         }
@@ -57,8 +41,7 @@ namespace Portalum.Zvt.UnitTest
         {
             var data = new byte[] { 0xF1, 0xF0 };
 
-            var bmpParser = this.GetBmpParser();
-            var length = bmpParser.GetDataLengthLL(data);
+            var length = BmpParser.GetDataLengthLL(data);
 
             Assert.AreEqual(10, length);
         }
